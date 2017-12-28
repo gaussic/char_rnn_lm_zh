b2faca5194f97d714c251d36269629e81d5f176f
gaussic <dzkangshu@hotmail.com>
2017-12-28 13:54:47 +0800

    test data

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from data_preprocessing import *
+
+train_dir = 'data/sanguoyanyi.txt'
+corpus = Corpus(train_dir)
+print("三国演义：", corpus)
+
+train_dir = 'data/weicheng.txt'
+corpus = Corpus(train_dir)
+print("围城：", corpus)
